df08fb69c600715c82925a2224b978b3659bc568
Ivo Zinenda <izinenda@gmail.com>
2019-02-22 22:11:48 +0200

    code restructed

--- assets/js/slick-init.js
+++ assets/js/slick-init.js
@@ -1,3 +1,12 @@
+$('.single-item').slick({
+  dots: true,
+  infinite: true,
+  arrows: false,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 3000,
+});
+
 $('.slick').slick({
     dots: true,
     infinite: false,
@@ -32,4 +41,42 @@ $('.slick').slick({
       // settings: "unslick"
       // instead of a settings object
     ]
-  });
+});
+
+
+$('.slick-clientes').slick({
+  dots: false,
+  infinite: true,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  slidesToShow: 6,
+  slidesToScroll: 3,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 5,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
