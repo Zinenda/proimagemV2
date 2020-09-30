6a0c6bf0add379d2dce9c612a30b511b5dfd09bd
Ivo Zinenda <izinenda@gmail.com>
2020-09-30 03:41:22 +0200

    side nav removed again and others scripts

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,8 @@
 $(document).ready(function(){
-  $(".dropdown-trigger").dropdown();
+  $('.sidenav').sidenav();
   $('.scrollspy').scrollSpy();
   $('.modal').modal();
+  $('.tooltipped').tooltip();
   $('.collapsible').collapsible();
   $('.materialboxed').materialbox();
-  
-  var $container = $('#masonry-grid');
-    // initialize
-    $container.masonry({
-      columnWidth: '.col',
-      itemSelector: '.col',
-  });
-
-  // layout Masonry after each image loads
-  $container.imagesLoaded().progress( function() {
-    $container.masonry('layout');
-  });
 });
